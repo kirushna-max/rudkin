36eb250728f9a2841fe0f9e0016f0a19f3d84be6
kirushna-max <shanduryakirushna@gmail.com>
2025-09-27 08:41:20 +0000

    opencv cpp waala

---
+++ Cppd2/build/CMakeFiles/circles.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kriss/Documents/repoclone/Cppd2/src/circles.cpp" "CMakeFiles/circles.dir/src/circles.cpp.o" "gcc" "CMakeFiles/circles.dir/src/circles.cpp.o.d"
+  "" "circles" "gcc" "CMakeFiles/circles.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Cppd2/build/CMakeFiles/circles.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/circles.dir/link.d"
+  "CMakeFiles/circles.dir/src/circles.cpp.o"
+  "CMakeFiles/circles.dir/src/circles.cpp.o.d"
+  "circles"
+  "circles.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/circles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
